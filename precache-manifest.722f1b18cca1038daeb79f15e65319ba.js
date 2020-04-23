d63752623ae72178c907c4a3c697df7a70236370
Anthony Cooper <iam@anthonycooper.me>
2020-04-23 14:05:24 -0500

    Updates

---
+++ precache-manifest.722f1b18cca1038daeb79f15e65319ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c24aa43f8d7164a7b739b707f11b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ac9029ed424708121da",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3d3a292245f36cc9791d",
+    "url": "/static/js/2.d3d82816.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3d82816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac9029ed424708121da",
+    "url": "/static/js/main.c63cce45.chunk.js"
+  },
+  {
+    "revision": "c0a66892c9d88e3c60e7",
+    "url": "/static/js/runtime-main.8e5dee85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
